213474c3be4eac985c91de0784304da1bf2843e5
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-07 10:42:43 +0900

    Update python basic.py

--- python basic.py
+++ python basic.py
@@ -1,5 +1,13 @@
-# 
+# data type
 値の型整数型(int), 浮動小数点型(float), 文字列型(str)
 
 # データ構造
 リスト、タプル、集合、辞書
+
+# text file 
+import csv
+
+with open("/Users/jimmy/Desktop/VAAK/Camera01.csv", "r") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
