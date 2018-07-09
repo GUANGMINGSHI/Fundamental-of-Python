fc4a8f98a559e56aab0ace0925ac29e566175dac
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-09 21:46:42 +0900

    Update 8. Function and Module.py

--- 8. Function and Module.py
+++ 8. Function and Module.py
@@ -1,1 +1,12 @@
+# define function
+def s(message):
+    # don't know this means
+    value = int(input(message))
+    if value < 0:
+        print("you are wrong.")
+    return value
 
+# use function you defined, no need input()
+bottom = s("bottom")
+high = s("high")
+print(bottom * high / 2)
