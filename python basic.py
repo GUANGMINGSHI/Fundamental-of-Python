146e42bbd5be1059ed5c7a8720aae1702cf9ccbb
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-09 15:29:35 +0900

    Update python basic.py

--- python basic.py
+++ python basic.py
@@ -1,12 +1,50 @@
+
 # data type
 値の型整数型(int), 浮動小数点型(float), 文字列型(str)
 
 # データ構造
 リスト、タプル、集合、辞書
 
-# text file 
-import csv
 
+"""control statement"""
+# specify data individually
+for num in [1,2,3,4,5]:
+    print(num)
+
+# specify data's range
+for num in range(10):
+    print("num=" + str(num))
+    print("num*num=" + str(num*num))
+
+# while: run n times until a defined condition is no longer met
+count = 5
+while count > 0:
+    print(str(count))
+    count -= 1
+
+# "break;": break the loop
+while True:
+    value = int(input("yen"))
+    if value == 0:
+        print("0 is inputted.")
+        break;
+    print(100 / value)
+
+# "continue": continue this loop
+remain = 5
+while remain > 0:
+    print(str(remain))
+    value = int(input("what?"))
+    if value == 0:
+        print("0 is inputted.")
+        continue;
+    print(100 /value)
+    remain -= 1
+
+    
+    
+"""text file"""
+import csv
 with open("/Users/jimmy/Desktop/VAAK/Camera01.csv", "r") as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
