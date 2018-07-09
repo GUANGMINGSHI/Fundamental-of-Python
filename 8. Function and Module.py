752151e4bab0e34a04107509924eb9ae44a68f9d
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-09 22:08:32 +0900

    Update 8. Function and Module.py

--- 8. Function and Module.py
+++ 8. Function and Module.py
@@ -1,12 +1,19 @@
-# define function
+## define function(1-one argument)
 def s(message):
     # don't know this means
     value = int(input(message))
     if value < 0:
         print("you are wrong.")
     return value
-
 # use function you defined, no need input()
 bottom = s("bottom")
 high = s("high")
 print(bottom * high / 2)
+
+## define function(2-many argument)
+def s(bottom, high):
+    area = bottom * high / 2
+    return area
+# use function you defined, no need input()
+s = s(3, 4)
+print(s)
