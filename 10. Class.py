91bccd92f60d53caa4a0393cd36cb84f8984d5a4
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-13 14:48:11 +0900

    Update 10. Class.py

--- 10. Class.py
+++ 10. Class.py
@@ -6,7 +6,8 @@ Two kinds of object's in Python's OOP model: class object and instance object.
 For exampel: student class (name, birthday, course), teacher class(name, birthday, lecture)
              Triangle class (bottom, height, area, edge; calc_area, calc_edge) 
 '''
-# define a class
+
+"""define a class"""
 class Dog:
   # class attribute
   species = 'mammal'
@@ -36,6 +37,36 @@ print(philo.description())
 print(mikey.speak("Gruff Gruff"))
 
 
+""" conbine two class in one class"""
+class Robot:
+    def __init__(self, n, c, w):
+        self.name = n
+        self.color = c
+        self.weight = w
+        
+    def introduce_self(self):
+        print("My name is " + self.name)
+r1 = Robot("Tom", "red", 30)
+r2 = Robot("Jerry", "blue", 40)
+
+class Person:
+    def __init__(self, n, p, i):
+        self.name = n
+        self.personality = p
+        self.is_sitting = i
+        
+    def sit_down(self):
+        self.is_sitting = True
+    
+    def stand_up(self):
+        self.is_sitting = False
+p1 = Person("Alice", "aggressive", False)
+p2 = Person("Becky", "talkative", True)
+
+p1.robot_owned = r1
+p2.robot_owned = r2
+
+p1.robot_owned.introduce_self()
 
 
 
