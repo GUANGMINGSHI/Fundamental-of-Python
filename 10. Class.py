6aae4f7c4f67b1eb4859e10bb4105605bf7d6868
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-13 12:40:17 +0900

    Update 10. Class.py

--- 10. Class.py
+++ 10. Class.py
@@ -1,35 +1,37 @@
 
 Two kinds of object's in Python's OOP model: class object and instance object.
 
-オブジェクトの設計をクラスという. Class consist of variable and method.
+'''
+オブジェクトの設計をクラスという. Class = variable + method.
 For exampel: student class (name, birthday, course), teacher class(name, birthday, lecture)
              Triangle class (bottom, height, area, edge; calc_area, calc_edge) 
-
-クラスに基づいて実際に作成した個々のものをオブジェクトやインスタンスという. For example: two Triangle class (h = 1, b = 1) or (h = 2, b = 2).
-
-every object is an instance of a class. For Example: Kevin is human.
-
+'''
 # define a class
 class Dog:
   # class attribute
   species = 'mammal'
   
-  # Constractor: which function's name same as class name. class 内の"__init__"を自動的に呼び出す. "__init__"can initialization instance.
-  def __init__(self, x1, x2):
-    self.num1 = x1
-    self.num2 = x2
+  # Constractor: which function's name same as class name. class 内の"__init__"を自動的に呼び出す. 
+  # "__init__"can initialization instance.
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
     
-  # Method: create same as function, but should add "self" before argument.
+  # Instance method: used to get the contents of an instance.
+  # create same as function; add "self" before argument.
   def description(self):
     return "{} is {} years old".format(self.name, self.age)
-  def speak(slef, sound):
+  def speak(self, sound):
     return "{} says {}".format(self.name, sound)
     
 # Create instance: class.(argument)
+# クラスに基づいて実際に作成した個々のものをオブジェクトやインスタンスという.
+# Every object is an instance of a class. For Example: Kevin is human.
+# For example: two Triangle class (h = 1, b = 1) or (h = 2, b = 2).
 philo = Dog("Philo", 5)
 mikey = Dog("Mikey", 6)
 
-# call method: instance.class()
+# call instance method: instance.class()
 print(philo.description())
 print(mikey.speak("Gruff Gruff"))
 
