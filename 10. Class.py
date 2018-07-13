d511de057fcdda3cecf5ba947a3c0a5241e86d0c
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-13 14:55:33 +0900

    Update 10. Class.py

--- 10. Class.py
+++ 10. Class.py
@@ -39,32 +39,34 @@ print(mikey.speak("Gruff Gruff"))
 
 """ conbine two class in one class"""
 class Robot:
-    def __init__(self, n, c, w):
-        self.name = n
-        self.color = c
-        self.weight = w
-        
-    def introduce_self(self):
-        print("My name is " + self.name)
-r1 = Robot("Tom", "red", 30)
-r2 = Robot("Jerry", "blue", 40)
+  def __init__(self, n, c, w):
+    self.name = n
+    self.color = c
+    self.weight = w
+    
+  def introduce_self(self):
+    print("My name is " + self.name)
+
+r1 = Robot("Tom", "red", 23)
+r2 = Robot("Jerry", "blue", 32)
 
 class Person:
-    def __init__(self, n, p, i):
-        self.name = n
-        self.personality = p
-        self.is_sitting = i
-        
-    def sit_down(self):
-        self.is_sitting = True
+  def __init__(self, n, p, i):
+    self.name = n
+    self.personality = p
+    self.is_sitting = i
     
-    def stand_up(self):
-        self.is_sitting = False
+  def sit_down(self):
+    self.is_sitting = True
+    
+  def stand_up(self):
+    self.is_sitting = False
+    
 p1 = Person("Alice", "aggressive", False)
 p2 = Person("Becky", "talkative", True)
 
 p1.robot_owned = r1
-p2.robot_owned = r2
+p1.robot_owned = r2
 
 p1.robot_owned.introduce_self()
 
