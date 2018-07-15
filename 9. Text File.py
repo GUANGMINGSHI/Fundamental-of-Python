a20acf6c550d1fae7f213be19b8b251ce188e27f
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-15 14:17:15 +0900

    Update 9. Text File.py

--- 9. Text File.py
+++ 9. Text File.py
@@ -1,25 +1,9 @@
 """
-text file (Jupyter notebook)
-file's input and output
+file operation take place three steps:
 1. open the file
 2. read or write the file
 3. close the file
 """
-
-# open as read--"r"
-file = open("/Users/jimmy/Desktop/Python/iris.csv", "r")
-# contain all in one variable
-#x = file.read(1)
-# divided into rows for each line
-lines = file.readlines()
-print(lines)
-file.close
-
-# use "with...open...as"
-with open("/Users/jimmy/Desktop/Python/iris.csv") as file:
-    for line in file:
-        line2 = line.strip("¥n")
-        print(line2)
 
 # 書き出しでファイルを開く, only "string"
 with open("/Users/jimmy/Desktop/Python/test.txt", "w") as file:
@@ -33,28 +17,19 @@ with open("/Users/jimmy/Desktop/Python/test.txt", "w") as file:
     file.write("is")
     file.writelines(topic_list)
 
-# read by rows
-file = open("/Users/jimmy/Desktop/Python/iris.csv")
-for line in file:
-    line2 = line.rstrip("¥n")
-    print(line2)
-file.close()
-
-# show the csv file last one rows
+# Question1: show the csv file last one rows
 with open("/Users/jimmy/Desktop/Python/iris.csv") as file:
     for line in file:
-        line2 = line.strip("¥n")
         # conver string to list
         line2 = line2.split(",")
         print(line2[-1])
   
-# write in another file, rather than show it.
+# Question2: write in another file, rather than show it.
 with open("/Users/jimmy/Desktop/Python/iris.csv") as file_in:
     with open("/Users/jimmy/Desktop/Python/iriss.csv", "w") as file_out:
         for line in file_in:
-            line2 = line.strip("¥n")
-            line_data = line2.split(",")
-            file_out.write(line_data[-1] + "¥n")
+            line_data = line.split(",")
+            file_out.write(line_data[-1])
      
    
 """ use module:csv"""
