48fc1971defacfdb05c9503a8178ec253a47ca11
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-15 12:11:28 +0900

    Update 12. Answer.py

--- 12. Answer.py
+++ 12. Answer.py
@@ -1,29 +1,18 @@
 #第12回：It's difficult.
+# 最大公約数を求める。ユークリッド互除法
+a = int(input("number a?"))
+b = int(input("number b?"))
 
-'''ユークリッド互除法'''
-a = int(input("a?"))
-b = int(input("b?"))
-r = a%b
-while r:
+if a > b:
+    c = a
     a = b
-    b = r
-    r = a % b
+    b = c
+while a != 0:
+    b = b % a
+    c = a
+    a = b
+    b = c
 print(b)
-
-
-'''数当てゲーム'''
-import random
-r = random.randrange(100)
-
-while True:#True表示一直循环直到满足break的条件
-    p = int(input("What's your number?"))#放在外面会形成无限循环
-    if r > p:#要有大小，才能有比较。
-        print("Smaller!!")
-    elif r < p:
-        print("Bigger!!")
-    else:
-        print("This is right!")
-        break
 
 '''会議の日程調整'''
 ＃第13回：例外処理、複雑な反復処理、関数の応用
