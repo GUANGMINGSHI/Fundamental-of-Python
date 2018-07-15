b1d8baaf87a03f14c9703f26acd9d49e84d0e116
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-15 16:15:23 +0900

    Update 12. Answer.py

--- 12. Answer.py
+++ 12. Answer.py
@@ -14,14 +14,3 @@ while a != 0:
     b = c
 print(b)
 
-'''会議の日程調整'''
-＃第13回：例外処理、複雑な反復処理、関数の応用
-for i in range(5):
-    value = int(input("Zhengshu?"))
-    try:
-        print(100 / value)
-    except:
-        print("Over")
-        break
-else:
-    print("This loop is over!")
