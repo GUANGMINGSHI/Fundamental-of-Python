647f0c8b4c55f99f2e6af327e84223e96540b40d
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-08-03 12:13:27 +0900

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -1,12 +1,3 @@
-# numpy: entire array, different from list.
-# a new kind of pyhton type, like float.
-import numpy as np
-height = np.array([1, 2, 3, 4, 5])
-print(type(height))
-weight = np.array([1, 2, 3, 4, 5])
-print(type(weight))
-sum = height + weight
-print(sum)
 #type coercion: change different data type to
 np.array([True, 1, 2]) + np.array([3, 4, False])
 # same as
@@ -19,8 +10,6 @@ np_2d = np.array([[1.73, 1.68, 1.71, 1.89, 1.79],
 # change 1 float to string, all of the elements will change too
 np_2d_string = np.array([[1.73, 1.68, 1.71, 1.89, 1.79],
                 [65.4, 59.2, 63.6, 88.4, "68.7"]])
-# use print(), if not, just print last one.
-print(type(np_2d_string))
 # select one row
 print(np_2d[1])
 print(np_2d[0][3])# or [0, 3]
@@ -28,7 +17,6 @@ print(np_2d[0][3])# or [0, 3]
 print(np_2d[:, 1:3])
 ## conda: means select all rows or cols
 print(np_2d[:, 0])
-
 
 # basic statistics
 np.mean(np[:,0])
@@ -43,3 +31,12 @@ np_city = np.column_stack((height, weight))
 
 # Heights of the goalkeepers use []
 gk_heights = np_heights[np_positions == "GK"]
+
+
+# 忘年会の予算
+import numpy as np
+x,y = input().split()
+# dtypeを指定する必要がある? Why?
+np_x = np.array(x, dtype='int')
+np_y = np.array(y, dtype='int')
+print(np_x * 6000 + np_y * 4000)
