f3f3a51a51c34886fa629a258e2ba24722a4e6d2
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-08-18 08:38:40 +0900

    Update 5. Text File.py

--- 5. Text File.py
+++ 5. Text File.py
@@ -31,40 +31,7 @@ with open("/Users/jimmy/Desktop/Python/iris.csv") as file_in:
             line_data = line.split(",")
             file_out.write(line_data[-1])
  
-# Question3: Adjustment of meeting schedule, 
-# カウンタをそれぞれ０で初期化
-count = []
-for i in range(32):
-    count.append(0)
 
-# create vip's list    
-vip = []
-
-with open("/Users/jimmy/Desktop/Python/days.csv") as file_in:
-    for line in file_in:
-        # 指定した末尾のものを除去する; strip: 指定した最初と末尾のものを除去する.
-        x = line.rstrip('\n')
-        days_list = x.split(",")
-        
-        # それぞれのデータについてカウンタに1をプラス
-        for i in days_list:
-            count[int(i)] += 1
-        # why len?
-        # 最初の人のデータが空だったら覚えておく
-        if len(vip) == 0:
-            vip = days
-# 最初の人が参加可能な日で最多のものをmに入れる
-m = 0
-# 最初の人が参加可能な日をそれぞれ見る
-for v in vip:
-    # その日の人数
-    c = count[int(v)]
-    # もし今までで最多の日を上回ったらmを更新する
-    if c > count[m]:
-        m = int(v)
-
-print(m)
-  
    
 """ csv module - csv file reading and writing. """
 # read csv file
@@ -90,7 +57,3 @@ with open('dict.csv', 'r') as f:
     reader = csv.DictReader(f)
     for row in reader:
         print(row)
-
-        
-"""using pandas to operate csv data"""
-camera_csv = pd.read_csv(filepath_or_buffer="/Users/jimmy/Desktop/VAAK/Camera01.csv", encoding="ms932", sep=",")
