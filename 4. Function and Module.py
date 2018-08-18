2bd3b4f09ba22b4cadcd265cd49c443e019e9006
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-08-18 14:52:37 +0900

    Update 4. Function and Module.py

--- 4. Function and Module.py
+++ 4. Function and Module.py
@@ -1,9 +1,24 @@
-'''define function(2 arguments)'''
-def s(bottom, high):
-#     you can docstring in here
-    area = bottom * high / 2
-#     return [expression list]
-    return area      
+'''
+determine a number is prime or not
+'''
+def get_number(text):
+    return int(input("please input your number?"))
+
+# call input function should("")
+a = get_number("")
+def prime(value):
+    for i in range(1, a+1):
+        if a % i == 0:
+            if (i != 1 and i != a):
+                print(a, "is not prime")
+                # use break to stop the loop
+                break
+    # loop also has else clause. this "else" belong to for not if
+    else:
+        print(a, "is prime")
+               
+print(prime(a))      
+
 
 '''map(function, iterable): apply a function to a list and get a new list.'''
 eur_jpy = 122.75
