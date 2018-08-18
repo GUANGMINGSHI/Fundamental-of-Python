0d2f9bb28f2346bf34dd16cc20b6e67719193404
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-08-18 22:06:44 +0900

    Update 6. Class.py

--- 6. Class.py
+++ 6. Class.py
@@ -10,11 +10,13 @@ For exampel: student class (name, birthday, course), teacher class(name, birthda
 
 '''define a simple class'''
 class Dog:
+# メソッドの文法は関数と同じ     
   pass
 # create instance of class
 Dog()
 Dog()
 
+# メソッドの使用：インスタンス名＋メソッド名
 a = Dog()
 b = Dog()
 
@@ -48,38 +50,3 @@ mikey = Dog("Mikey", 6)
 # call instance method: instance.method()
 print(philo.description())
 print(mikey.speak("Gruff Gruff"))
-
-
-""" conbine two class in one class"""
-class Robot:
-  def __init__(self, n, c, w):
-    self.name = n
-    self.color = c
-    self.weight = w
-    
-  def introduce_self(self):
-    print("My name is " + self.name)
-
-r1 = Robot("Tom", "red", 23)
-r2 = Robot("Jerry", "blue", 32)
-
-class Person:
-  def __init__(self, n, p, i):
-    self.name = n
-    self.personality = p
-    self.is_sitting = i
-    
-  def sit_down(self):
-    self.is_sitting = True
-    
-  def stand_up(self):
-    self.is_sitting = False
-    
-p1 = Person("Alice", "aggressive", False)
-p2 = Person("Becky", "talkative", True)
-
-p1.robot_owned = r1
-p1.robot_owned = r2
-
-p1.robot_owned.introduce_self()
-
